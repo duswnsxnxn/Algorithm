e6ef4b878825d977129639052dd5d18b8109c4a9
duswnsxnxn <65449174+duswnsxnxn@users.noreply.github.com>
2023-04-05 17:36:29 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class Solution {
     public String solution(String cipher, int code) {
+        
         StringBuilder sb = new StringBuilder();
         for (int i = code; i <= cipher.length(); i += code) {
             sb.append(cipher.charAt(i - 1));
